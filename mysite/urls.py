110638b7e6a849c7f2c54bebd22819b0c66ba230
darkgallow <r.g.rouco@hotmail.com>
2017-09-10 16:45:26 -0300

    correcion errores

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,9 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
     url(r'', include('blog.urls'))
-=======
-    url(r'', include('blog.urls')),
->>>>>>> 6052b96a2032128a731f1875014bccda0a6d34b3
 ]
